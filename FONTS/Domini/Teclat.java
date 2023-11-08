5e235ad0ba9cb78064a14d48cd62a0c3a8d48a38
pol.plana <123639572+pol-33@users.noreply.github.com>
2023-11-08 15:18:28 +0100

    Teclat Implementació
    Implementació del funcionament principal de la classe

--- FONTS/Domini/Teclat.java
+++ FONTS/Domini/Teclat.java
@@ -2,22 +2,51 @@ package Domini;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import ControladorsDomini.ControladorAlgoritme;
 
 public class Teclat {
 
-    //Identificador de teclat
-    private int id;
-    private ArrayList<Character> tecles;
+    protected ArrayList<Character> tecles;
+    protected Integer num_files;
+    protected Integer num_columnes;
+    protected Entrada entrada;
 
     // Creadora
-    public Teclat(ArrayList<Character> tecles, HashMap<String, Integer> lpf) {
-        this.tecles = tecles;
+    public Teclat(ArrayList<Character> lletres, Entrada entrada) {
+        this.entrada = entrada;
+        num_files = 3;
+        num_columnes = lletres.size()/num_files;
+        if (lletres.size()%num_files != 0) num_columnes++;
+        // Obtenir controlador algoritme
+        ControladorAlgoritme ctrlAlgoritme = ControladorAlgoritme.obtenirInstancia();
+        this.tecles = ctrlAlgoritme.calcularDistribucioDuesMans(entrada.getLPF(), lletres, num_files, num_columnes);
     }
 
     // Getters
-    public int getId() {
-        return id;
+    public ArrayList<Character> getTecles() {
+        return tecles;
     }
+
+    // Setters
+
+    // Defineix el nombre de files del teclat, reajustant el nombre de columnes i la distribució de tecles
+    public void setNumFiles(Integer num_files) {
+        this.num_files = num_files;
+        num_columnes = tecles.size()/num_files;
+        if (tecles.size()%num_files != 0) num_columnes++;
+        regenerarTeclat(tecles, entrada.getLPF());
+    }
+
+    // Funcions publiques
+
+    // Recalcula la distribució de tecles del teclat
+    public void regenerarTeclat(ArrayList<Character> lletres, HashMap<String, Integer> lpf) {
+        // Obtenir controlador algoritme
+        ControladorAlgoritme ctrlAlgoritme = ControladorAlgoritme.obtenirInstancia();
+        this.tecles = ctrlAlgoritme.calcularDistribucioDuesMans(lpf, lletres, num_files, num_columnes);
+    }
+
+
 
 
 }
