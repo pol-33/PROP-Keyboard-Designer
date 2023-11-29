3869d4a1d47d01371dec88028d71cac8c2d2db7d
pol.plana <123639572+pol-33@users.noreply.github.com>
2023-11-30 00:04:17 +0100

    Conversió Tests JUnit 5 a JUnit 4
    S'han fet els canvis necessaris per a canviar la implementació dels tests perquè passin d'utilitzar JUnit 5 a JUnit 4.
    
    - Incloses les llibreries adients a la carpeta lib i eliminades les incorrectes
    - A les classes de test s'han canviat els imports propis de JUnit 5 pels ara necessaris de JUnit 4
    - S'ha canviat l'ordre dels paràmetres de certes funcions, que canvia en funció de la versió de JUnit (principalment a JUnit 5 el paràmetre missatge va en última posició, mentre que a JUnit 4 va en primera)
    - S'ha afegit l'equivalència de la funció de JUnit 5 assertDoesNotThrow() perquè no existeix a JUnit 4
    - S'ha modificat el Makefile eliminant les llibreries antigues i afegint-hi les noves
    
    Tots els canvis dins les classes de tipus Test han estat canvis trivials per a fer compatible el canvi de versió de JUnit. No s'ha implementat cap funcionalitat no prevista prèviament en els testos.

--- FONTS/src/test/LPFTest.java
+++ FONTS/src/test/LPFTest.java
@@ -1,13 +1,12 @@
 package test;
 
 import domini.classes.LPF;
-import org.junit.jupiter.api.Test;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
-import static org.junit.jupiter.api.Assertions.*;
 
-class LPFTest {
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class LPFTest {
 
     @Test
     public void provaConstructorAmbFrequenciesPositives() {
@@ -15,8 +14,14 @@ class LPFTest {
         contingutEntrada.put("paraula1", 1);
         contingutEntrada.put("paraula2", 2);
 
-        assertDoesNotThrow(() -> new LPF("LPFTest", 1, contingutEntrada, 2),
-                "No hauria d'haver excepció amb frequencies positives");
+        // Equivalent del mètode de JUnit 5 assertDoesNotThrow() amb JUnit 4
+        try {
+            new LPF("LPFTest", 1, contingutEntrada, 2);
+            // Si no hi ha excepcions, el test passa
+        } catch (Exception e) {
+            // Si n'hi ha, es produeix un fail
+            fail("Unexpected exception: " + e.getMessage());
+        }
     }
 
 
@@ -26,6 +31,6 @@ class LPFTest {
         contingutEntrada.put("paraula1", -1); // Frequencia negativa
 
         Exception excepcio = assertThrows(Exception.class, () -> new LPF("LPFTest", 1, contingutEntrada, 2));
-        assertEquals("Una paraula no pot tenir frequencia negativa", excepcio.getMessage(), "El missatge d'excepció hauria de ser correcte");
+        assertEquals("El missatge d'excepció hauria de ser correcte", excepcio.getMessage(), "Una paraula no pot tenir frequencia negativa");
     }
 }
