9d2a9c2275ff2af673437f8b7bf83a2b863c89bb
Lluis Mir <107417761+lluismir@users.noreply.github.com>
2023-12-17 16:26:42 +0100

    funcions basiques gestorUsuaris
    pendents

--- FONTS/src/persistencia/classes/GestorUsuaris.java
+++ FONTS/src/persistencia/classes/GestorUsuaris.java
@@ -1,6 +1,6 @@
 package persistencia.classes;
 
-import java.io.BufferedReader;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -12,6 +12,8 @@ public class GestorUsuaris {
     // ---------------------------------------------------------------------------- //
     private String filePath = "../../DATA/usuaris.csv";
     private Path path = Paths.get(filePath);
+
+    // ---------------------------------------------------------------------------- //
 
     public ArrayList<String> obtenirUsernames(String username) {
         ArrayList<String> usernames = new ArrayList<>();
@@ -28,4 +30,55 @@ public class GestorUsuaris {
 
         return usernames;
     }
+
+
+    public ArrayList<String> obtenirPasswordUsuari(String username) {
+        ArrayList<String> contrasenyas = new ArrayList<>();
+        File file = path.toFile();
+
+        if (!file.exists()) {
+            return contrasenyas;
+        }
+
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                // asumim que el format de cada linia sera "usuario,contraseña"
+                String[] userData = line.split(",");
+                if (userData.length > 1 && userData[0].equals(username)) {
+                    contrasenyas.add(userData[1]); // fiquem el password a la llista si coincideix el username
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return contrasenyas;
+    }
+
+    public void eliminarUsuari(String nomUsuari) throws IOException {
+        File inputFile = path.toFile();
+        ArrayList<String> users = new ArrayList<>();
+
+        // llegeix l'arxiu i guarda els usuarios en una llista, excepte el que volem eliminar
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFile))) {
+            String currentLine;
+
+            while ((currentLine = reader.readLine()) != null) {
+                // asumim que cada linia tindra el forma "usuario,contraseña"
+                String[] userData = currentLine.split(",");
+                if (!userData[0].equals(nomUsuari)) {
+                    users.add(currentLine);
+                }
+            }
+        }
+
+        // tornem a escriu l'arxiu com una llista actualutzada
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(inputFile, false))) {
+            for (String userLine : users) {
+                writer.write(userLine);
+                writer.newLine();
+            }
+        }
+    }
 }
+
