f6a7e361845a5e89ec3734cc598b2b6f73d8aed6
pol.plana <123639572+pol-33@users.noreply.github.com>
2023-12-23 20:02:07 +0100

    Definicio Look and feel
    El look and feel del programa es defineix al del sistema

--- FONTS/src/drivers/Main.java
+++ FONTS/src/drivers/Main.java
@@ -2,8 +2,19 @@ package drivers;
 
 import presentacio.controladors.ControladorPresentacio;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
+        try {
+            // Set system look and feel
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            //UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+            //UIManager.setLookAndFeel("com.apple.laf.AquaLookAndFeel");
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+
         ControladorPresentacio.startAplicacio();
     }
 }
